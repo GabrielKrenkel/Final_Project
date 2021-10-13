9874ccf689364a945f4b6c70bec2cc1f85792942
Gabriel Krenkel <83610092+GabrielKrenkel@users.noreply.github.com>
2021-10-13 17:15:30 -0300

    chancer

---
+++ QUAC_SYS/client/src/pages/InitialPage/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css"
+
+export function InitialPage() {
+
+
+
+    return (
+        <>
+            <body class="w-100 sans-serif">
+
+                <header className="head w-100 dt pa3 ph5-ns bg-near-white">
+                    <div className="dtc v-mid tl w-50">
+                        <a className="dib f5 f4-ns fw6 mt0 mb1 link black-70 dim" href="./">Tachyons</a>
+
+                    </div>
+                    <nav className="dc dtc v-mid w-100 tr">
+                        <a className="f6 fw6 dim link black-70 mr1 mr3-m mr4-l dib" href="./">Docs</a>
+                        <a className="f6 fw6 dim link black-70 mr1 mr3-m mr4-l dib" href="./">Componets</a>
+                        <a className="f6 fw6 dim link black-70 mr1 mr3-m mr4-l dn dib-ns" href="./">Github</a>
+                    </nav>
+                </header>
+
+                <main className="w-100">
+                                   
+
+                </main>
+
+                <footer>
+
+                </footer>
+
+            </body>
+
+        </>
+    )
+}
